4d60fbb4c9079b42be1e99d1169fd2eebed6480d
Jetty <hanbingdn@163.com>
2019-08-23 15:15:45 +0800

    change a little bit layout

--- resources/views/wselessons/create.blade.php
+++ resources/views/wselessons/create.blade.php
@@ -1,23 +1,39 @@
 @extends('layout')
 
 @section('content')
-	<h1>Create a New Lesson</h1>
+	<h1 class="title">Create a New Lesson</h1>
 
 	<form method="POST" action="/wselessons">
 		{{ csrf_field() }}
-		<div>
-			<input type="text" name="unit" placeholder="unit"/>
-		</div>
+		<div class="field">
+		<label class="label" for="unit">Unit</label>
 
-		<div>
-			<input type="text" name="lesson" placeholder="lesson"/>
+		<div class="control">
+			<input type="text" class="input" name="unit" placeholder="unit" />
 		</div>
+	</div>
 
-		<div>
-			<textarea name="content" placeholder="content"></textarea>
+	<div class="field">
+		<label class="label" for="lesson">Lesson</label>
+
+		<div class="control">
+			<input type="text" class="input" name="lesson" placeholder="lesson" />
 		</div>
+	</div>
 
-		<div>
-			<button type="submit">Create a Lesson</button>
+	<div class="field">
+		<label class="label" for="content">Content</label>
+
+		<div class="control">
+			<textarea name="content" class="textarea" ></textarea>
+		</div>
+	</div>
+
+	<div class="field">
+		<div class="control">
+			<button type="submit" class="button is-link">Create Lesson</button>
+		</div>
+	</div>
+</form>
 	</form>
 @endsection
--- resources/views/wselessons/index.blade.php
+++ resources/views/wselessons/index.blade.php
@@ -1,14 +1,22 @@
 @extends('layout')
 
 @section('content')
-	<table>
-	@foreach ($lessons as $lesson)
-		<tr>
-			<td>{{ $lesson->unit }}</td>
-			<td>{{ $lesson->lesson }}</td>
-			<td>{{ $lesson->content }}</td>
-		</tr>
-	@endforeach
-	</table>
+
+<div class="columns">
+	<div class="column is-1">Unit</div>
+	<div class="column is-1">Lesson</div>
+	<div class="column">Points</div>
+	<div class="column">Shen</div>
+	<div class="column">Kae</div>
+	<div class="column">Sofi</div>
+</div>
+
+@foreach ($lessons as $lesson)
+<div class="columns">
+  	<div class="column is-1">{{ $lesson->unit }}</div>
+  	<div class="column is-1">{{ $lesson->lesson }}</div>
+  	<div class="column">{{ $lesson->content }}</div>
+</div>
+@endforeach
 	
 @endsection
